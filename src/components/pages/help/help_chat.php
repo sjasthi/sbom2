1773038be9f8ab1a65329c580fc488bc7bf5cbc2
siqbal11269 <siqbal11269@gmail.com>
2022-06-21 00:00:26 -0500

    Update help_chat.php

--- src/components/pages/help/help_chat.php
+++ src/components/pages/help/help_chat.php
@@ -5,8 +5,5 @@
 
   include("../../../../index.php");
   include("help_left_menu.php");
+  echo '<iframe width="800" height="600" src="http://localhost:3000/s/test" frameborder="0"></iframe>';
  ?>
-
-<div class="wrap">
-  <h3> TODO: Help - Chat. </h3>
-</div>
