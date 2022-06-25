2163366847083366691c17daf92a2f25e851b480
Brent <71100424+Brent-W-Anderson@users.noreply.github.com>
2022-06-25 10:51:57 -0500

    clarify parameters to frontend

--- src/components/pages/search/options/search_who.php
+++ src/components/pages/search/options/search_who.php
@@ -10,34 +10,51 @@
                 'SELECT * FROM Ownership
                 WHERE app_type LIKE "'.$val.'"
                 AND app_name LIKE "'.$searchName.'%"';
+
+                echo 
+                    '<h3>
+                        <div>
+                            SELECT <span> * </span> FROM <span> Ownership </span>
+                        </div>
+                        
+                        <div>
+                            WHERE <span> app_type = </span>
+                            <span><i><u>'.$val.'</u></i></span>
+                        </div>
+
+                        <div>
+                            AND <span> app_name = </span>
+                            <span><i><u>'.$searchName.'</u></i></span>
+                        </div>
+                    </h3>';            
         } else {
             $sql =
                 'SELECT * FROM Ownership
                 WHERE app_type LIKE "'.$search.'%"';
+            
+            if( $search === '' ) {
+                echo 
+                    '<h3>
+                        <div>
+                            SELECT <span> * </span> FROM <span> Ownership </span>
+                        </div>
+                    </h3>';
+            } else {
+                echo 
+                    '<h3>
+                        <div>
+                            SELECT <span> * </span> FROM <span> Ownership </span>
+                        </div>
+                        
+                        <div>
+                            WHERE <span> app_type = </span>
+                            <span><i><u>'.$search.'</u></i></span>
+                        </div>
+                    </h3>';
+            }
         }
 
         $data = $GLOBALS['db'] -> query( $sql );
-
-        if( $search === '' ) {
-            echo 
-            '<h3>
-                <div>
-                    SELECT <span> * </span> FROM <span> Ownership </span>
-                </div>
-            </h3>';
-        } else {
-            echo 
-            '<h3>
-                <div>
-                    SELECT <span> * </span> FROM <span> Ownership </span>
-                </div>
-                
-                <div>
-                    WHERE <span> app_type = </span>
-                    <span><i><u>'.$search.'</u></i></span>
-                </div>
-            </h3>';
-        }
 
         return $data;
     }
