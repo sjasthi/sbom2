65174f68ddd9d39de6f0c6e98f02473aa267087e
Brent <71100424+Brent-W-Anderson@users.noreply.github.com>
2022-06-25 08:22:16 -0500

    add search (functionality): "who <app_type>" <app_type> = color

--- src/components/pages/search/who_where.php
+++ src/components/pages/search/who_where.php
@@ -1,35 +1,41 @@
 
 <?php
+  include("buildTable.php"); // buildTable();
+
   // default to "where" searches when nothing is provided (where/who)
   function who_where() {
-    include("buildTable.php"); // buildTable();
-
     $search = $_POST["searchVal"];
     $firstWord = strtok( $search, ' ' );
 
     if( !strcasecmp( $firstWord, 'who' ) ) { // Ownership table
-      $val = strstr( $search, ' ' );
+      $val = trim( strstr( $search, ' ' ) );
+      $sql =
+        'SELECT * from Ownership
+        WHERE app_type LIKE "'.$val.'%"';
+      $data = $GLOBALS['db'] -> query( $sql );
+
+      if( $val === '' ) {
+        $val = '*';
+      }
 
       echo 
-      '<h3>
-        <span>Query: <i>"Ownership"</i></span>
+        '<h3>
+          <span>Query: <i>"Ownership"</i></span>
+          <div>
+            WHERE <span> app_type = </span>
+            <span><i><u>'.$val.'</u></i></span>
+          </div>
+        </h3>';
 
-        <div>
-          WHERE <span> app_name = </span>
-          <span><i><u>'.$val.'</u></i></span>
-        </div>
-      </h3>';
-
-      $data = array( 0 => array( 'who' => $val ) );
       buildTable( $data );
 
       return $data;
     } else { // App_Components table
-      // search component name first, then app name.
       $sql =
-      'SELECT * from apps_components
-      WHERE cmpt_name LIKE "'.$search.'%"
-      OR app_name LIKE "'.$search.'%"';
+        'SELECT * from apps_components
+        WHERE cmpt_name LIKE "'.$search.'%"
+        OR app_name LIKE "'.$search.'%"';
+      $data = $GLOBALS['db'] -> query( $sql );
 
       echo 
         '<h3>
@@ -41,7 +47,6 @@
           </div>
         </h3>';
       
-      $data = $GLOBALS['db'] -> query( $sql );
       buildTable( $data );
 
       return $data;
