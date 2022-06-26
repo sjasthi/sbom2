d03b5b7a4af53baa5fd6aa4b13cbdfb4d10b0aff
Nathan A. Souer <github@nathansouer.com>
2022-06-26 16:28:04 +0000

    CRUD operations for system app set complete.

--- src/components/pages/admin/admin_system_app_sets.php
+++ src/components/pages/admin/admin_system_app_sets.php
@@ -88,6 +88,7 @@
 
 
 <?php
+  // Set the current system app set.
   if (isset($_POST['current_app_set'])) {
     if ( $_POST['current_app_set']  == '0' ) {
       $sys_app_sets = $db->prepare('update preferences set value = "0" where name = "ACTIVE_APP_SET";');
@@ -107,17 +108,38 @@
     }
   }
 
-  if (isset($_POST['new_app_set_id'])) {
+  // Delete the select app set.
+  if (isset($_POST['delete_app_set'])) {
     $delete_app_sets = $db->prepare('delete from app_sets where app_set_id = ?;');
-    $delete_app_sets->bind_param('s',$_POST['new_app_set_id']);
+    $delete_app_sets->bind_param('s',$_POST['delete_app_set']);
     if(!$delete_app_sets->execute()) {
       echo '<p style="background: red; color: white; font-size: 2rem;">ERROR: '.$db->error.'</p>';
     } else {
       echo '<p style="background: green; color: white; font-size: 2rem;">Deleting System App Set: '.$_POST['delete_app_set'].'</p>';
     }
-
-
   }
 
+  // Add or replace an app set.
+  if (isset($_POST['new_app_set_id'])) {
+    // Delete the existing rows for that app_set_id
+    $replace_app_sets = $db->prepare('delete from app_sets where app_set_id = ?;');
+    $replace_app_sets->bind_param('s',$_POST['new_app_set_id']);
+    if(!$replace_app_sets->execute()) {
+      echo '<p style="background: red; color: white; font-size: 2rem;">ERROR: '.$db->error.'</p>';
+    } else {
+      echo '<p style="background: green; color: white; font-size: 2rem;">Adding or replacing System App Set: '.$_POST['new_app_set_id'].'</p>';
+    }
+    // Insert neccesary rows into the app_set table
+    $app_id_array = explode(",", $_POST['new_app_set_app_list'] );
+    foreach ($app_id_array as $each_app_id) {
+      $app_sets_insert = $db->prepare('insert into app_sets ( app_set_id, app_set_name, app_id ) values ( ?, ?, ? );');
+      $app_sets_insert->bind_param('sss',$_POST['new_app_set_id'],$_POST['new_app_set_name'],$each_app_id);
+      if(!$app_sets_insert->execute()) {
+        echo '<p style="background: red; color: white; font-size: 2rem;">ERROR: '.$db->error.'</p>';
+      } else {
+        echo '<p style="background: green; color: white; font-size: 2rem;">Adding '.$each_app_id.' to System App Set: '.$_POST['new_app_set_id'].'</p>';
+      }
+    }
+  }
 
 ?>
