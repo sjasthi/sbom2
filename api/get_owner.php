a99d15d17f384739a7bb128035a04fd45432f946
ri1178lw <ri1178lw@go.minnstate.edu>
2022-06-30 18:56:28 -0500

    Created get_owner.php
    Master file to handle all get_owner api's for both application and component input

--- api/get_owner.php
+++ api/get_owner.php
@@ -8,14 +8,24 @@
      *          component_id = digits only.
      *          component_name = Alpha,digits, space and certain special characters.
      *          component_version = digits, space and certain special characters
+     * 
+     *          app_id = digits only.
+     *          app_name = Alpha,digits, space and certain special characters.
+     *
      *
      * SAMPLE URL INPUTS
-     * http://localhost/sbom2/api/get_owner_by_component.php?component_id=755954
-     * http://localhost/sbom2/api/get_owner_by_component.php?component_id=67702376
-     * http://localhost/sbom2/api/get_owner_by_component.php?component_name=LTS JSON Libray
-     * http://localhost/sbom2/api/get_owner_by_component.php?component_name=kassandra HttpClient
-     * http://localhost/sbom2/api/get_owner_by_component.php?component_version=7.5
-     * http://localhost/sbom2/api/get_owner_by_component.php?component_version=9.4.76.v70700237
+     * Application input
+     * http://localhost/sbom2/api/get_owner.php?component_id=755954
+     * http://localhost/sbom2/api/get_owner.php?component_id=67702376
+     * http://localhost/sbom2/api/get_owner.php?component_name=LTS JSON Libray
+     * http://localhost/sbom2/api/get_owner.php?component_name=kassandra HttpClient
+     * http://localhost/sbom2/api/get_owner.php?component_version=7.5
+     * http://localhost/sbom2/api/get_owner.php?component_version=9.4.76.v70700237
+     * //Component input
+     * http://localhost/sbom2/api/get_owner.php?app_id=76074884
+     * http://localhost/sbom2/api/get_owner.php?app_id=944965237
+     * http://localhost/sbom2/api/get_owner.php?app_name=LTS JSON L
+     * http://localhost/sbom2/api/get_owner.php?app_name=Techno Com
      * Output:  The module outputs data as a json object. The json object also includes HTTP
      *          response code and count of rows parameters passed and data name value pairs.
      *
@@ -98,7 +108,54 @@
         else {
             invalidResponse("Invalid Request");
         }
-    }else {
+    } else if(isset($_GET['app_id'])) {
+        $app_id = $_GET['app_id'];
+
+        if(!empty($app_id) && preg_match('/^\d*$/', $app_id)) {
+            $apiFunctions = new apiUtility();
+            $processor = $apiFunctions->get_owner_app_id($app_id);
+            $data = [];
+            $count = 0;
+            if($processor!==false && $processor->num_rows > 0) {
+                $count = $processor->num_rows;
+                while($row  = $processor->fetch_assoc()){
+                    $data[] = $row;
+                }
+            }
+            $res = [];
+            response(200, $count, $app_id, $data);
+        }
+        else if (isset($app_id) && empty($app_id)) {
+            invalidResponse("Invalid or Empty input");
+        }
+        else {
+            invalidResponse("Invalid Request");
+        }
+    } else if(isset($_GET['app_name'])) {
+        $app_name = $_GET['app_name'];
+
+        if(!empty($app_name) && preg_match('/^[\d A-Za-z +:-]*$/', $app_name)) {
+            $apiFunctions = new apiUtility();
+            $processor = $apiFunctions->get_owner_app_name($app_name);
+            $data = [];
+            $count = 0;
+            if($processor!==false && $processor->num_rows > 0) {
+                $count = $processor->num_rows;
+                while($row  = $processor->fetch_assoc()){
+                    $data[] = $row;
+                }
+            }
+
+            $res = [];
+            response(200, $count, $app_name, $data);
+        }
+        else if (isset($app_name) && empty($app_name)) {
+            invalidResponse("Invalid or Empty input");
+        }
+        else {
+            invalidResponse("Invalid Request");
+        }
+    } else{
         invalidResponse("Invalid Request");
     }
 
