9048b665b00ec8983abe5a695c6f8867e112d65f
Anthony Flowers <>
2022-07-02 16:27:02 -0500

    Added functionality to create app sets. App sets can also be selected to become the system app set.

--- src/components/pages/applications/app_functions.php
+++ src/components/pages/applications/app_functions.php
@@ -18,4 +18,82 @@ function showAppsAsChecklist($db){
   }
 }
 
+function getNextAppsetId($db){
+  $sql_query_last_appset_id = '
+    SELECT app_set_id
+    FROM app_sets
+    ORDER BY app_set_id DESC
+    LIMIT 1;
+  ';
+  $query_result = $db->query($sql_query_last_appset_id);
+  if($query_result->num_rows < 1){
+    return 1;
+  }
+  return $query_result->fetch_row()[0] + 1;
+}
+
+function createSystemAppset($db, $apps, $appset_name){
+  $appset_id = getNextAppsetId($db);
+  $sql_create_system_appset = '
+    INSERT INTO app_sets (app_set_id, app_set_name, app_id)
+    VALUES (?,?,?);
+  ';
+  try{
+    $db->autocommit(true);
+    $appset_insert = $db->prepare($sql_create_system_appset);
+    foreach($apps as $app_id){
+      $appset_insert->bind_param('iss', $appset_id, $appset_name, $app_id);
+      $appset_insert->execute();
+    }
+    return $db->commit();
+  } finally {
+    $db->autocommit(true);
+  }
+}
+
+function showAppsetsAsTable($db){
+    $sql_query_appsets = '
+      SELECT app_set_id, app_set_name, app_id
+      FROM app_sets
+      ORDER BY app_set_id ASC;
+    ';
+    $query_appsets_result = $db->query($sql_query_appsets);
+    if($query_appsets_result->num_rows < 1){
+      // show err
+      return;
+    }
+    $appset_id = NULL;
+    $appset_list = [];
+    while($app_from_appset = $query_appsets_result->fetch_assoc()){
+
+      if($appset_id != $app_from_appset['app_set_id']){
+        if($appset_id != NULL){
+          ?>
+          <script type="text/javascript">
+          let radio_btn = document.getElementById("<?php echo $appset_id ?>");
+          radio_btn.value = <?php echo '"'.implode(',', $appset_list).'"'; ?>;
+          </script>
+          <?php
+          $appset_list = [];
+        }
+        echo '<tr>';
+        $appset_id = $app_from_appset['app_set_id'];
+        echo '<td><input id="'.$app_from_appset['app_set_id'].'" type="radio" name="appset_radio"></td>';
+        echo'<td>'.$app_from_appset['app_set_name'].'</td>';
+      } else {
+        echo '<td></td>';
+        echo '<td></td>';
+      }
+      echo'<td>'.$app_from_appset['app_id'].'</td>';
+      echo '</tr>';
+      $appset_list[] = $app_from_appset['app_id'];
+    }
+    ?>
+    <script type="text/javascript">
+    radio_btn = document.getElementById("<?php echo $appset_id ?>");
+    radio_btn.value = <?php echo '"'.implode(',', $appset_list).'"'; ?>;
+    </script>
+    <?php
+}
+
 ?>
--- src/components/pages/applications/app_page.php
+++ src/components/pages/applications/app_page.php
@@ -1,11 +1,13 @@
 <?php
 
 $nav_selected = "APPLICATIONS";
+$left_selected = "APPLIST";
 $tabTitle = "SBOM - APPLICATIONS";
 $app_checkbox_name = "checkboxApps";
 
 include("../../../../index.php");
 include("app_functions.php");
+include("app_left_menu.php");
 
 global $db;
 $DEFAULT_SCOPE_FOR_RELEASES = getScope($db);
@@ -13,10 +15,42 @@ $DEFAULT_SCOPE_FOR_RELEASES = getScope($db);
 ?>
 
 <div class="wrap">
-  <h3>Applications</h3>
+  <h3>Select BOM Apps:</h3>
   <form class="appSetForm" action="app_page.php" method="POST">
+    <button type="submit" name="user_submit">Set My Apps</button>
+    <?php
+    if($_SESSION['admin']){
+      echo '<button type="submit" name="system_submit">Set System Apps</button>';
+      echo '<button type="submit" name="appset_submit">Create Appset</button>';
+      echo '<input type="text" name="appset_name" placeholder="appset name">';
+    }
+    isset($_POST[$app_checkbox_name]) ? $apps = $_POST[$app_checkbox_name] : $apps = [];
+
+    if(isset($_POST['user_submit'])){
+      setUserAppSetCookies($apps);
+    }
+    if($_SESSION['admin']){
+      if(isset($_POST['system_submit'])){
+        if(setSystemAppSet($db, implode(',', $apps))){
+          echo '<p style="background: green; color: white; font-size: 2rem;">Set system app set.</p>';
+        } else {
+          echo '<p style="background: red; color: black; font-size: 2rem;">Error setting system app set.</p>';
+        }
+      } else if (isset($_POST['appset_submit'])){
+        if(!isset($_POST['appset_name']) || $_POST['appset_name'] == ''){
+          echo '<p style="background: red; color: black; font-size: 2rem;">Specify an app set name.</p>';
+        } else {
+          if(createSystemAppset($db, $apps, $_POST['appset_name'])){
+            echo '<p style="background: green; color: white; font-size: 2rem;">Created new system app set.</p>';
+          } else {
+            echo '<p style="background: red; color: black; font-size: 2rem;">Error creating a new system app set.</p>';
+          }
+        }
+      }
+    }
+
+    ?>
     <fieldset>
-      <legend>Select BOM Apps:</legend>
       <table class="datatable table">
         <thead>
           <tr id="table-first-row">
@@ -32,25 +66,7 @@ $DEFAULT_SCOPE_FOR_RELEASES = getScope($db);
         </tbody>
       </table>
     </fieldset>
-    <button type="submit" name="user_submit">Set Apps</button>
-    <?php
-    if($_SESSION['admin']){
-      echo '<button type="submit" name="system_submit">Set System Apps</button>';
-    }
-    ?>
   </form>
-  <?php
-  isset($_POST[$app_checkbox_name]) ? $apps = $_POST[$app_checkbox_name] : $apps = [];
-
-  if(isset($_POST['user_submit'])){
-    setUserAppSetCookies($apps);
-  }
-  if($_SESSION['admin'] && isset($_POST['system_submit'])){
-    if(setSystemAppSet($db, implode(',', $apps))){
-      echo '<p style="background: green; color: white; font-size: 2rem;">Set system app set.</p>';
-    }
-  }
-  ?>
   <script type="text/javascript">
   $('#check-all').click(function(){
     if($('#check-all')[0].checked){
