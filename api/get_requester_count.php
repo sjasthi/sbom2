0e69c9c32d9bf3906ff116b3c044296187b7032a
ri1178lw <ri1178lw@go.minnstate.edu>
2022-07-06 19:32:22 -0500

    added get_requester_count.php

---
+++ api/get_requester_count.php
@@ -0,0 +1,74 @@
+<?php
+    /**
+     * Purpose: API module get_owner.php provides information about the requester,
+     *          and application owner given either application or component information.
+     *
+     * Input:   supported input parameters are 'component_name, 'component_id', 
+     *          'component version', 'app_id', 'and app_name'. Both the
+     *          parameters can be used as a single input or passed as a combined unit.
+     *          component_id = digits only.
+     *          component_name = Alpha,digits, space and certain special characters.
+     *          component_version = digits, space and certain special characters
+     * 
+     *          app_id = digits only.
+     *          app_name = Alpha,digits, space and certain special characters.
+     *
+     *
+     * SAMPLE URL INPUTS
+     * Component input
+     * http://localhost/sbom2/api/get_owner.php?component_id=755954
+     * Output:  The module outputs data as a json object. The json object also includes HTTP
+     *          response code and count of rows parameters passed and data name value pairs.
+     *
+     * Error Conditions: response code of http 400 is generated when system detects an error condition.
+     *                   component_id, component_name, component_version can generate "Invalid request"
+     *                   or "Invalid or empty request" for unsupported characters.
+     *
+     * @author Shahid Iqbal, Isaac Hentges, Nathan Lantaigne-Goetsch, Abdulsalam Geddi
+     *
+     * The apiUtility class is for various helper functions for the php pages.
+     */
+    require("./apiUtility.php");
+
+    if(isset($_GET['component_id'])) {
+        $component_id = $_GET['component_id'];
+
+        if(!empty($component_id) && preg_match('/^\d*$/', $component_id)) {
+            $apiFunctions = new apiUtility();
+            $processor = $apiFunctions->get_owner_component_id($component_id);
+            $data = [];
+            $count = 0;
+            if($processor!==false && $processor->num_rows > 0) {
+                $count = $processor->num_rows;
+                while($row  = $processor->fetch_assoc()){
+                    $data[] = $row;
+                }
+            }
+            $res = [];
+            response(200, $count, $component_id, $data);
+        }
+        else if (isset($component_id) && empty($component_id)) {
+            invalidResponse("Invalid or Empty input");
+        }
+        else {
+            invalidResponse("Invalid Request");
+        }
+    } else{
+        invalidResponse("Invalid Request");
+    }
+
+
+    function invalidResponse($message) {
+        response(400, $message, NULL, NULL);
+    }
+
+    function response($responseCode, $message, $string, $data) {
+        // Locally cache results for two hours
+        header('Cache-Control: max-age=7200');
+        // JSON Header
+        header('Content-type:application/json;charset=utf-8');
+        http_response_code($responseCode);
+        $response = array("response_code" => $responseCode, "Records" => $message, "Parameter Value" => $string, "data" => $data);
+        $json = json_encode($response, JSON_PRETTY_PRINT);
+        echo $json;
+    }
