dda54988dfbbd9245aa7285955437f199fb315b1
Brent <71100424+Brent-W-Anderson@users.noreply.github.com>
2022-07-12 23:39:21 -0500

    adjust naming

--- src/db/createBiggerBOM/sql/app_components.sql
+++ src/db/createBiggerBOM/sql/app_components.sql
@@ -1,10 +1,10 @@
 
 INSERT INTO `apps_components` (`line_id`, `red_app_id`, `cmpt_id`, `cmpt_name`, `cmpt_version`, `app_id`, `app_name`, `app_version`, `license`, `status`, `requester`, `monitoring_id`, `monitoring_digest`, `issue_count`) VALUES
 (1, '76074884', '944965238', 'Unicode for C Sharp (Unicode4C)', '67.9', '76074884', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
-(2, '944965238', '944965236', 'child 1', '67.9', '944965238', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
-(3, '944965238', '944965236', 'child 2', '67.9', '944965238', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
-(4, '944965238', '944965236', 'child 3', '67.9', '944965238', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
-(5, '944965238', '944965236', 'child 4', '67.9', '944965238', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
+(2, '944965238', '944965236', 'child', '67.9', '944965238', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
+(3, '944965238', '944965236', 'child', '67.9', '944965238', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
+(4, '944965238', '944965236', 'child', '67.9', '944965238', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
+(5, '944965238', '944965236', 'child', '67.9', '944965238', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
 (6, '944965237', '944965230', 'kassandra/xerces-c', '67.9', '944965237', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
 (7, '944965237', '944965239', 'kassandra Directory Studio', '67.9', '944965237', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
 (8, '944965239', '944965230', 'kassandra Xalan-C++', '67.9', '944965239', 'LTS JSON L', '9.9', 'Unicode License V7', 'Approved', 'Tierra Von', '76654', 'na', 0),
