40b944fe8c04f050543f31a6e1c0e9107dacc898
MSI\gobre <gobrent21@gmail.com>
2022-07-19 23:02:15 -0500

    allow adjustable factors. 10,000 rows is too big for phpMyAdmin --drop to 2,000 for large_BOM.sql

--- src/db/createBiggerBOM/php/buttonSaveFiles.php
+++ src/db/createBiggerBOM/php/buttonSaveFiles.php
@@ -25,23 +25,33 @@ SET time_zone = "+00:00";
 -- Database: `sbom_db`
 --
 
+USE sbom_db;
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `apps_components`
 --
 
-DROP TABLE IF EXISTS `apps_components`;
+';
+
+    // add button cases for bigger BOMs here..
+    if( isset( $_POST['button'] ) ) {
+        $button = $_POST['button'];
+
+        switch( $button ) {
+            case "100": {
+                echo "Createed 100 new BOMS";
+
+$dropTable =
+'DROP TABLE IF EXISTS `apps_components`;
 CREATE TABLE IF NOT EXISTS `apps_components` (
     `line_id` int(10) NOT NULL,
     `red_app_id` varchar(10) NOT NULL,
-    `red_app_id2` varchar(10) NOT NULL,
     `cmpt_id` varchar(10) NOT NULL,
-    `cmpt_id2` varchar(10) NOT NULL,
     `cmpt_name` varchar(100) NOT NULL,
     `cmpt_version` varchar(50) NOT NULL,
     `app_id` varchar(100) NOT NULL,
-    `app_id2` varchar(100) NOT NULL,
     `app_name` varchar(10) NOT NULL,
     `app_version` varchar(50) NOT NULL,
     `license` varchar(120) DEFAULT NULL,
@@ -55,16 +65,7 @@ CREATE TABLE IF NOT EXISTS `apps_components` (
 --
 -- Dumping data for table `apps_components`
 --
-
 ';
-
-    // add button cases for bigger BOMs here..
-    if( isset( $_POST['button'] ) ) {
-        $button = $_POST['button'];
-
-        switch( $button ) {
-            case "100": {
-                echo "Createed 100 new BOMS";
                 
                 $saveFile = fopen( "sql/small_100.sql", 'w' );
                 $str = createSQL( factorOfTen( createTinyBOM() ) );
@@ -91,12 +92,40 @@ CREATE TABLE IF NOT EXISTS `apps_components` (
                 
                 $GLOBALS['db'] -> query( $str );
 
-                $str = $strPrepend.$str;
+                $str = $strPrepend.$dropTable.$str;
                 fwrite( $saveFile, $str );
             } break;
 
             case "1,000": {
                 echo "Createed 1,000 new BOMS";
+
+$dropTable = 
+'DROP TABLE IF EXISTS `apps_components`;
+CREATE TABLE IF NOT EXISTS `apps_components` (
+    `line_id` int(10) NOT NULL,
+    `red_app_id` varchar(10) NOT NULL,
+    `red_app_id2` varchar(10) NOT NULL,
+    `cmpt_id` varchar(10) NOT NULL,
+    `cmpt_id2` varchar(10) NOT NULL,
+    `cmpt_name` varchar(100) NOT NULL,
+    `cmpt_version` varchar(50) NOT NULL,
+    `app_id` varchar(100) NOT NULL,
+    `app_id2` varchar(100) NOT NULL,
+    `app_name` varchar(10) NOT NULL,
+    `app_version` varchar(50) NOT NULL,
+    `license` varchar(120) DEFAULT NULL,
+    `status` varchar(10) DEFAULT NULL,
+    `requester` varchar(50) DEFAULT NULL,
+    `monitoring_id` varchar(10) DEFAULT NULL,
+    `monitoring_digest` varchar(100) DEFAULT NULL,
+    `issue_count` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `apps_components`
+--
+
+';
                 
                 $saveFile = fopen( "sql/medium_1000.sql", 'w' );
                 $str =
@@ -133,14 +162,45 @@ CREATE TABLE IF NOT EXISTS `apps_components` (
                 
                 $GLOBALS['db'] -> query( $str );
 
-                $str = $strPrepend.$str;
+                $str = $strPrepend.$dropTable.$str;
                 fwrite( $saveFile, $str );
             } break;
 
-            case "10,000": {
-                echo "Createed 10,000 new BOMS";
+            case "2,000": {
+                echo "Createed 2,000 new BOMS";
+
+$dropTable = 
+'DROP TABLE IF EXISTS `apps_components`;
+CREATE TABLE IF NOT EXISTS `apps_components` (
+    `line_id` int(10) NOT NULL,
+    `red_app_id` varchar(10) NOT NULL,
+    `red_app_id2` varchar(10) NOT NULL,
+    `red_app_id3` varchar(10) NOT NULL,
+    `cmpt_id` varchar(10) NOT NULL,
+    `cmpt_id2` varchar(10) NOT NULL,
+    `cmpt_id3` varchar(10) NOT NULL,
+    `cmpt_name` varchar(100) NOT NULL,
+    `cmpt_version` varchar(50) NOT NULL,
+    `app_id` varchar(100) NOT NULL,
+    `app_id2` varchar(100) NOT NULL,
+    `app_id3` varchar(100) NOT NULL,
+    `app_name` varchar(10) NOT NULL,
+    `app_version` varchar(50) NOT NULL,
+    `license` varchar(120) DEFAULT NULL,
+    `status` varchar(10) DEFAULT NULL,
+    `requester` varchar(50) DEFAULT NULL,
+    `monitoring_id` varchar(10) DEFAULT NULL,
+    `monitoring_digest` varchar(100) DEFAULT NULL,
+    `issue_count` int(3) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `apps_components`
+--
+
+';
                 
-                $saveFile = fopen( "sql/large_10000.sql", 'w' );
+                $saveFile = fopen( "sql/large_2000.sql", 'w' );
                 $str =
                     createSQL(
                         addLayer( // add another layer (6 levels)
@@ -148,7 +208,7 @@ CREATE TABLE IF NOT EXISTS `apps_components` (
                                 factorOfTen( // 10,000
                                     factorOfTen( // 1,000
                                         factorOfTen( createTinyBOM() ) // 100 (default: 2 levels of depth)
-                                    )
+                                    ), 2
                                 ), 2
                             ), 3
                         )
@@ -182,7 +242,7 @@ CREATE TABLE IF NOT EXISTS `apps_components` (
                 
                 $GLOBALS['db'] -> query( $str );
 
-                $str = $strPrepend.$str;
+                $str = $strPrepend.$dropTable.$str;
                 fwrite( $saveFile, $str );
             } break;
         }
