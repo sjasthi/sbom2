f4c2a4e9ac52009589311c773e5b51fea786fd19
Brent <71100424+Brent-W-Anderson@users.noreply.github.com>
2022-08-09 17:40:14 -0500

    Update search_what.php
    remove parameters display to ui.

--- src/components/pages/search/options/search_what.php
+++ src/components/pages/search/options/search_what.php
@@ -7,30 +7,30 @@
             AND issue_count > 0';
         $data = $GLOBALS['db'] -> query( $sql );
 
-        if( $search === '' ) {
-            echo 
-            '<h3>
-                <div>
-                    SELECT <span> * </span> FROM <span> app_components </span>
-                </div>
-            </h3>';
-        } else {
-            echo 
-            '<h3>
-                <div>
-                    SELECT <span> * </span> FROM <span> app_components </span>
-                </div>
+//         if( $search === '' ) {
+//             echo 
+//             '<h3>
+//                 <div>
+//                     SELECT <span> * </span> FROM <span> app_components </span>
+//                 </div>
+//             </h3>';
+//         } else {
+//             echo 
+//             '<h3>
+//                 <div>
+//                     SELECT <span> * </span> FROM <span> app_components </span>
+//                 </div>
 
-                <div>
-                    WHERE <span> app_name = </span>
-                    <span><i><u>'.$search.'</u></i></span>
-                </div>
+//                 <div>
+//                     WHERE <span> app_name = </span>
+//                     <span><i><u>'.$search.'</u></i></span>
+//                 </div>
 
-                <div>
-                    AND <span> issue_count > 0 </span>
-                </div>
-            </h3>';
-        }
+//                 <div>
+//                     AND <span> issue_count > 0 </span>
+//                 </div>
+//             </h3>';
+//         }
         
         return $data;
     }
