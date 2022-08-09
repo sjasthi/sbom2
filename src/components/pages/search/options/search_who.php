04a515836414e59225d100571df72aebbb863251
Brent <71100424+Brent-W-Anderson@users.noreply.github.com>
2022-08-09 17:41:52 -0500

    Update search_who.php
    remove parameters display to ui.

--- src/components/pages/search/options/search_who.php
+++ src/components/pages/search/options/search_who.php
@@ -11,47 +11,47 @@
                 WHERE app_type LIKE "'.$val.'"
                 AND app_name LIKE "'.$searchName.'%"';
 
-                echo 
-                    '<h3>
-                        <div>
-                            SELECT <span> * </span> FROM <span> Ownership </span>
-                        </div>
+//                 echo 
+//                     '<h3>
+//                         <div>
+//                             SELECT <span> * </span> FROM <span> Ownership </span>
+//                         </div>
                         
-                        <div>
-                            WHERE <span> app_type = </span>
-                            <span><i><u>'.$val.'</u></i></span>
-                        </div>
+//                         <div>
+//                             WHERE <span> app_type = </span>
+//                             <span><i><u>'.$val.'</u></i></span>
+//                         </div>
 
-                        <div>
-                            AND <span> app_name = </span>
-                            <span><i><u>'.$searchName.'</u></i></span>
-                        </div>
-                    </h3>';            
+//                         <div>
+//                             AND <span> app_name = </span>
+//                             <span><i><u>'.$searchName.'</u></i></span>
+//                         </div>
+//                     </h3>';            
         } else {
             $sql =
                 'SELECT * FROM Ownership
                 WHERE app_type LIKE "'.$search.'%"';
             
-            if( $search === '' ) {
-                echo 
-                    '<h3>
-                        <div>
-                            SELECT <span> * </span> FROM <span> Ownership </span>
-                        </div>
-                    </h3>';
-            } else {
-                echo 
-                    '<h3>
-                        <div>
-                            SELECT <span> * </span> FROM <span> Ownership </span>
-                        </div>
+//             if( $search === '' ) {
+//                 echo 
+//                     '<h3>
+//                         <div>
+//                             SELECT <span> * </span> FROM <span> Ownership </span>
+//                         </div>
+//                     </h3>';
+//             } else {
+//                 echo 
+//                     '<h3>
+//                         <div>
+//                             SELECT <span> * </span> FROM <span> Ownership </span>
+//                         </div>
                         
-                        <div>
-                            WHERE <span> app_type = </span>
-                            <span><i><u>'.$search.'</u></i></span>
-                        </div>
-                    </h3>';
-            }
+//                         <div>
+//                             WHERE <span> app_type = </span>
+//                             <span><i><u>'.$search.'</u></i></span>
+//                         </div>
+//                     </h3>';
+//             }
         }
 
         $data = $GLOBALS['db'] -> query( $sql );
